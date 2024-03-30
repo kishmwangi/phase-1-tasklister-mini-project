a18c45d331c2d308f49e7827186b4aa761bb970f
kish mwangi <kishmwangi31@gmail.com>
2024-03-30 15:29:02 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = documephase-1-tasklister-mini-projectnt.querySelector('form');
+
+  //submit even listenner to newTaskForm
+
+  newTaskForm.addEventListener('submit' (e) => {
+    e.preventDefault();
+
+
+    const newToDo = e.target.querySelector('#new-task-description').value
+
+    //pass in newToDo variable 
+
+    buildToDo(newToDo);
+
+    newTaskForm.reset();
+
+  })
 });
+
+ //when user submits, we pass in the targeted input into buildToDo function
+
+function buildToDo(newToDo){
+  const taskLi = document.createElement('li');
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'x';
+  taskLi.textContent = newToDo;
+  taskLi.appendChild(deleteButton);
+  document.querySelector('#tasks').appendChild(taskLi);
+  deleteButton.addEventListener('click', handleDelete);
+}
+
+ function handleDelete(e){
+  e.target.parentNode.remove();
+}
